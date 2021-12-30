18e4a8385961bc2229943fa13a098ef752bc78c3
Joshua Wang <joshuathesmart427@gmail.com>
2021-12-31 00:12:54 +0800

    init

---
+++ my_first_app/lib/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'color.dart' as color;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: color.AppColor.homePageBackground,
+        body: Container(
+          padding: const EdgeInsets.only(top: 70, left: 30),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Training',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: color.AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_back_ios,
+                    size: 20,
+                    color: color.AppColor.homePageIcons,
+                  ),
+                  Icon(
+                    Icons.calendar_today_outlined,
+                    size:20,
+                    color: color.AppColor.homePageIcons,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    size:20,
+                    color: color.AppColor.homePageIcons,
+                  ),
+                  
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
